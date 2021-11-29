5d764c09c4130c9f948cd6ace431a070812f40f8
ndo22 <ocernosek@gmail.com>
2021-11-29 20:04:56 +0100

    Added Favourites Page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {ChuckjokesService} from '../api/chuckjokes.service';
+import { ChuckjokesService } from '../api/chuckjokes.service';
 import { LoadingController } from '@ionic/angular';
+import { Storage } from '@capacitor/storage';
 
 
 @Component({
@@ -10,42 +11,55 @@ import { LoadingController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  myoutput:String = 'Joke will apperar after click on button';
+  myoutput: string = 'Joke will apperar after click on button';
+  myid: string = "";
   loadingDialog: any;
 
-constructor(private chuckService: ChuckjokesService, public loadingController: LoadingController) 
-{
+  constructor(private chuckService: ChuckjokesService, public loadingController: LoadingController) {
 
-}
+  }
 
-public btnClicked():void
-{
-  this.presentLoading();
-  this.chuckService.getRandom().subscribe( (data) => 
-    {
+  public async btnFavClicked(): Promise<void> {
+
+    if (this.myid != "" || this.myoutput != 'Joke will apperar after click on button') {
+      try {
+        alert(this.myid);
+        alert(this.myoutput);
+        await Storage.set({
+          key: this.myid,
+          value: this.myoutput
+        });
+        alert(`Added To Favourites`);
+      } catch (reason) {
+        alert(reason);
+        console.log(reason);
+      }
+    }
+  }
+
+  public btnClicked(): void {
+    this.presentLoading();
+    this.chuckService.getRandom().subscribe((data) => {
       var text = data['value']['joke'];
+      this.myid = data['value']['id'];
       text.toString();
-      if(text.includes('&quot;'))
-      {
+      if (text.includes('&quot;')) {
         this.myoutput = text.replaceAll('&quot;', '\"');
       }
-      else
-      {
+      else {
         this.myoutput = text;
       }
       this.loadingDialog.dismiss();
     });
   }
 
-
-async presentLoading() 
-{
-  this.loadingDialog = await this.loadingController.create(
-  {
-    message: 'Thinking ...', 
-  });
-await this.loadingDialog.present();
-}
+  async presentLoading() {
+    this.loadingDialog = await this.loadingController.create(
+      {
+        message: 'Thinking ...',
+      });
+    await this.loadingDialog.present();
+  }
 
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { KeyboardStyle } from '@capacitor/keyboard';
+import { Storage } from '@capacitor/storage';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +9,65 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  
+  favourites = ["You dont have any favourite joke right now"];
+  keys : string[];
 
-  constructor() {}
+  constructor() {
+   // this.loadFav();
+    //setName();
+  }
+
+
+  async loadFav() {   
+    
+    //checkName(this.keys, this.favourites);
+
+    await Storage.keys().then(result => {
+      this.keys = result.keys;
+      // rest of script
+    });
+
+    alert(`Hello ${this.keys[1]}!`);
+
+    for (let index = 0; index < this.keys.length; index++) {
+      const { value } = await Storage.get({ key: this.keys[index] });
+      alert(`Hello ${value}!`);
+      this.favourites[index] = value;
+    }
+  }
 
 }
+
+const setName = async () => {
+  await Storage.set({
+    key: 'name',
+    value: 'Max',
+  });
+};
+
+const checkName = async (keys : string[], favourites : string[]) => {
+  alert(`clicked!`);
+
+  await Storage.keys().then(result => {
+    keys = result.keys;
+    // rest of script
+  });
+
+  alert(`Hello ${keys[1]}!`);
+
+  for (let index = 0; index < keys.length; index++) {
+    const { value } = await Storage.get({ key: keys[index] });
+    alert(`Hello ${value}!`);
+    favourites[index] = value;
+  }
+
+
+  //const { value } = await Storage.get({ key: 'name' });
+
+  //alert(`Hello ${value}!`);
+};
+
+const removeName = async () => {
+  await Storage.remove({ key: 'name' });
+};
